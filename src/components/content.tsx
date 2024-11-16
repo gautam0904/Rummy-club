d2758c310a037f5a72bc3b1828c36825862fc635
gautam0904 <malaviyagautam0942@gmail.com>
2024-11-16 14:59:17 +0530

    img done

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,12 +4,12 @@ const Content = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
-    'public/image-1.jpg',
-    'public/image-2.jpg',
-    'public/image-3.jpg',
-    'public/image-4.jpg',
-    'public/image-5.jpg',
-    'public/image-6.jpg',
+    '/image-1.jpg',
+    '/image-2.jpg',
+    '/image-3.jpg',
+    '/image-4.jpg',
+    '/image-5.jpg',
+    '/image-6.jpg',
   ];
 
   const nextSlide = () => {
@@ -73,36 +73,36 @@ const Content = () => {
       </div>
 
       <div className="raw mt-3 content-gradiant d-flex align-items-center">
-        <div className="raw d-flex container align-items-center">
+        <div className="raw d-flex container align-items-center py-1">
           <div className='col-lg-8'>
             <p>
               A popular online card game with a unique twist is called Teen Patti. The game involves strategy, quick thinking, and a bit of luck, making it exciting and engaging. Through online platforms, players can join games anytime and compete against friends and other players around the world. With user-friendly interface, secure transactions, and exciting gameplay, online Teen Patti offers an authentic experience that keeps players coming back, blends tradition with the convenience of modern technology.
             </p>
           </div>
           <div className='col-lg-4'>
-            <img className='content-img img-fluid' src="../../public/three card.png" alt="" />
+            <img className='content-img img-fluid' src="/three card.png" alt="" />
           </div>
         </div>
 
       </div>
 
       <div className="mt-3 container  d-flex justify-content-between align-items-center">
-        <img className='three-img img-fluid mx-2 col-lg-4' src="../../public/Thampi Book.png" alt="" />
-        <img className='three-img img-fluid mx-2 col-lg-4' src="../../public/table.png" alt="" />
-        <img className='three-img img-fluid mx-2 col-lg-4' src="../../public/The most trustworthy.png" alt="" />
+        <img className='three-img img-fluid mx-2 col-lg-4' src="/Thampi Book.png" alt="" />
+        <img className='three-img img-fluid mx-2 col-lg-4' src="/table.png" alt="" />
+        <img className='three-img img-fluid mx-2 col-lg-4' src="/The most trustworthy.png" alt="" />
       </div>
 
-
       <div className="raw mt-3 content-gradiant d-flex align-items-center">
-        <div className="raw d-flex container align-items-center">
+        <div className="raw d-flex container align-items-center py-1">
           <div className='col-lg-4'>
-            <img className='content-img img-fluid' src="../../public/Be a master.png" alt="" />
+            <img className='content-img img-fluid' src="/Be a master.png" alt="" />
           </div>
-          <div className='col-lg-8'></div>
-          <p>
-            The traditional version of Teen Patti involves each player receiving three cards and a bet on who has the strongest hand. Similar to poker, the aim is to hold a better hand than the other players. The game can be played with or without a villain, adding suspense as …
-          </p>
-        </div>
+          <div className='col-lg-8'>
+            <p>
+            The traditional version of Teen Patti involves each player receiving three cards and a bet on who has the strongest hand. Similar to poker, the aim is to hold a better hand than the other players. The game can be played with or without a villain, adding suspense as
+            </p>
+          </div>
+        </div>       
       </div>
     </div>
 
